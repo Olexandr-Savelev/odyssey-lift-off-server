3365017cee5766257584207eee2d6aa4f92bdd4e
Olexandr-Savelev <a.savelev.krm@gmail.com>
2022-08-20 12:33:52 +0300

    remove unnecessary string from apollo server

--- src/index.js
+++ src/index.js
@@ -18,16 +18,3 @@ server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(` 🚀  Server is running!
   🔉  Listening on port ${url}`)
 })
-
-
-
-startApolloServer(typeDefs, resolvers)
-
-
-// server.listen().then(() => {
-//   console.log(`
-//     🚀  Server is running!
-//     🔉  Listening on port 4000
-//     📭  Query at http://localhost:4000
-//   `);
-// });
